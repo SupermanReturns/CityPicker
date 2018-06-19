245e87f5913a6ab9b5f95c95cca5890e5603e59e
Superman <703353105@qq.com>
2018-06-19 14:34:47 +0800

    first commit

---
+++ CityPicker/SDateView.h
@@ -0,0 +1,13 @@
+//
+//  SDateView.h
+//  CityPicker
+//
+//  Created by Superman on 2018/6/19.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDateView : UIView
+
+@end
---
+++ CityPicker/SDateView.m
@@ -0,0 +1,21 @@
+//
+//  SDateView.m
+//  CityPicker
+//
+//  Created by Superman on 2018/6/19.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import "SDateView.h"
+
+@implementation SDateView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
